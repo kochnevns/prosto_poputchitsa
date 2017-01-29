591ecf63b78c6bdead4261f696631ceff164943c
Nikita <kochnevns@icloud.com>
2017-01-29 22:58:11 +0300

    keklol

---
+++ popup.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+        function getDOM() {
+          console.log(document)
+          return document.querySelector('.Body').textContent;
+        }
+
+          chrome.tabs.executeScript(null,{code:"var x = document.querySelector('.Body').textContent; x"}, (results) => {
+            var sentencies = results[0].split('.');
+            var currentSent = 0;
+            function tryToPlayNext() {
+              if (speechSynthesis.speaking) {
+                setTimeout(tryToPlayNext, 2000);
+              }
+              else {
+                if (currentSent < sentencies.length) {
+                  speechSynthesis.cancel();
+                  speechSynthesis.speak(new SpeechSynthesisUtterance(sentencies[currentSent].trim()));
+                  currentSent++;
+                  tryToPlayNext();
+                }
+
+
+              }
+            }
+          tryToPlayNext();
+
+          })
+
+
+
+});
