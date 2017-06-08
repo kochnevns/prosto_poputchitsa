e6a4f5f68f6f8f23d99a485d6373e7e9244f6d83
Nikita <kochnevns@icloud.com>
2017-06-08 17:43:33 +0300

    piizdaaaa

--- popup.js
+++ popup.js
@@ -1,4 +1,27 @@
+const bloodyStreamMap = {
+  'Можно сдохнуть': 'can_die.mp3',
+  'А меня не прёт': 'no_pret.mp3',
+  'Гантеля'       : 'gantela.mp3',
+  'Быть плохим заебись' : 'be_bad_zaebis.mp3',
+  'Куртец'        : 'kurtec.mp3'
+};
+
+
 document.addEventListener('DOMContentLoaded', function () {
+
+
+          let $trackList = document.querySelector('.tracklist');
+
+            for ( let title in bloodyStreamMap) {
+                let trackTemplate  = `<li>${title}</li>`;
+                let $li = document.createElement('li');
+                $li.innerHTML = trackTemplate;
+                $trackList.appendChild($li);
+                $li.addEventListener('click', (e) =>{
+                  document.querySelector('source').src = 'audio/' + bloodyStreamMap[title];
+                  document.querySelector('audio').load();
+                })
+            }
 
 
         document.querySelector('button').addEventListener('click', function() {
